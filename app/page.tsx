94e798f1c21d7eb42f749b825e39f37e77225be3
Mahfuzur Rahman <99127099+Adoxcol@users.noreply.github.com>
2024-09-23 19:52:59 +0600

    2

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
+
         <FloatingNav
         navItems={navItems}
         />
